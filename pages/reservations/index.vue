ca2f3bea1b01da5066c7b8d7722c446bfec2d0d3
daisuke shiraishi <kaiyujin@gmail.com>
2018-06-05 01:29:19 +0900

    2018

--- pages/reservations/index.vue
+++ pages/reservations/index.vue
@@ -60,7 +60,7 @@
       </v-container>
     </v-content>
     <v-footer color="indigo" app>
-      <span class="white--text">&copy; 2017</span>
+      <span class="white--text">&copy; 2018</span>
     </v-footer>
   </v-app>
 </template>
