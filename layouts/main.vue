6645f3422269ce2a248f5df7a794bca355042e94
daisuke shiraishi <kaiyujin@gmail.com>
2018-06-08 00:37:53 +0900

    add shopName

--- layouts/main.vue
+++ layouts/main.vue
@@ -6,15 +6,6 @@
       app
     >
       <v-list dense>
-        <v-list-tile @click="reservations">
-          <v-list-tile-action>
-            <v-icon>subtitles</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>予約</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list>
         <v-list-group
           v-for="item in items"
           v-model="item.active"
@@ -27,13 +18,13 @@
               <v-list-tile-title>{{ item.title }}</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
-          <v-list-tile v-for="subItem in item.items" :key="subItem.title" @click="settings(subItem.action)">
+          <v-list-tile v-for="subItem in item.items" :key="subItem.title" @click="link(subItem.action)">
             <v-list-tile-content>
               <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list-group>
-        </v-list>
+
         <v-list-tile @click="logout">
           <v-list-tile-action>
             <v-icon>clear</v-icon>
@@ -52,12 +43,14 @@
           <v-icon>keyboard_arrow_left</v-icon>
         </v-btn>
         <v-btn outline color="white" @click="today">
-        Today
+          Today
         </v-btn>
         <v-btn outline color="white" @click="next_day">
           <v-icon>keyboard_arrow_right</v-icon>
         </v-btn>
       </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>{{shopName}}</v-toolbar-title>
     </v-toolbar>
     <v-content>
       <v-container fluid>
@@ -78,67 +71,78 @@
 </template>
 
 <style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+  html {
+    font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+  *, *:before, *:after {
+    box-sizing: border-box;
+    margin: 0;
+  }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+  .button--green {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #3b8070;
+    color: #3b8070;
+    text-decoration: none;
+    padding: 10px 30px;
+  }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+  .button--green:hover {
+    color: #fff;
+    background-color: #3b8070;
+  }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
+  .button--grey {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #35495e;
+    color: #35495e;
+    text-decoration: none;
+    padding: 10px 30px;
+    margin-left: 15px;
+  }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+  .button--grey:hover {
+    color: #fff;
+    background-color: #35495e;
+  }
 </style>
 
 <script>
   import moment from 'moment';
+
   export default {
     data: () => ({
       drawer: null,
       date: moment(new Date()).format('YYYY/MM/DD'),
+      shopName: 'Restaurant hoobar',
       items: [
+        {
+          action: 'subtitles',
+          title: '予約',
+          active: true,
+          items: [
+            {title: 'カレンダー', action: '/reservations'},
+            {title: '予約検索', action: '/reservations/search'},
+          ]
+        },
         {
           action: 'settings',
           title: '設定',
-          active: true,
+          active: false,
           items: [
-            { title: '店舗基本情報' ,action: 'shop'},
-            { title: 'テーブル' ,action: 'table'},
-            { title: 'WEB予約' ,action: 'reservation'}
+            {title: '店舗基本情報', action: '/settings/shop'},
+            {title: 'テーブル', action: '/settings/table'},
+            {title: 'WEB予約', action: '/settings/reservation'}
           ]
         }
       ]
@@ -146,24 +150,24 @@ html {
     props: {
       source: String
     },
-		methods :{
-			reservations() {
+    methods: {
+      reservations() {
         this.$router.push('/reservations/')
       },
-			settings(action) {
-        this.$router.push('/settings/' + action)
+      link(action) {
+        this.$router.push(action)
       },
-      logout(){
+      logout() {
         this.$router.push('/')
       },
-      prev_day(){
+      prev_day() {
         this.date = moment(this.date).subtract(1, 'days').format('YYYY/MM/DD')
       },
-      today(){
+      today() {
         this.date = moment(new Date()).format('YYYY/MM/DD')
       },
-      next_day(){
-			  this.date = moment(this.date).add(1, 'days').format('YYYY/MM/DD')
+      next_day() {
+        this.date = moment(this.date).add(1, 'days').format('YYYY/MM/DD')
       }
     }
   }
