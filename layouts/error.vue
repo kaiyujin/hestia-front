6472e0d3fb8a88d2ad4c7ada942ca2263036b3c0
Daisuke Shiraishi <kaiyujin@gmail.com>
2018-06-26 15:16:15 +0900

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">{{ $t("error.page_not_found") }}</h1>
+    <h1 v-else>{{ $t("error.internal_server_error") }}</h1>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['error'],
+    layout: 'default'
+  }
+</script>
