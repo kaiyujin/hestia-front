d66b782d591ea6dddedf43085c2da9772a2b7384
daisuke shiraishi <kaiyujin@gmail.com>
2018-07-05 00:19:59 +0900

    add login process

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,31 @@
   <div id="app">
     <v-container>
       <v-layout row class="text-xs-center">
-        <v-flex xs4 />
+        <v-flex xs4/>
         <v-flex xs4 class="blue lighten-5">
           <v-container class="text-xs-center">
             <v-card flat>
               <v-card-title primary-title>
-                <h4>Login</h4>
+                <h4>Login{{errorMessage}}</h4>
               </v-card-title>
               <v-form ref="form">
                 <v-text-field prepend-icon="person"
-                              v-model="Username"
-                              label="Username"
+                              v-model="username"
+                              label="username"
                               required
                               v-validate="'required|max:255'"
-                              :error-messages="errors.collect('Username')"
-                              data-vv-name="Username"
+                              :error-messages="errors.collect('username')"
+                              data-vv-name="username"
                 ></v-text-field>
-                <v-text-field prepend-icon="lock" name="Password" label="Password" type="password"></v-text-field>
+                <v-text-field prepend-icon="lock"
+                              v-model="password"
+                              name="password"
+                              label="password"
+                              v-validate="'required|max:255'"
+                              :error-messages="errors.collect('password')"
+                              type="password"
+                              data-vv-name="password">
+                </v-text-field>
                 <v-card-actions>
                   <v-btn primary round large block color="blue lighten-3" @click="submit">Login</v-btn>
                 </v-card-actions>
@@ -32,33 +40,43 @@
 </template>
 
 <script>
+  import axios from 'axios';
+
   export default {
 
     data: () => ({
-      Username: '',
-      Password: '',
-      dictionary: {
-        attributes: {
-          //Username: 'E-mail Address'
-        },
-        custom: {
-          password: {
-            required: () => 'pass can not be empty',
-            max: 'The name field may not be greater than 100 characters'
-            // custom messages
-          }
-        }
-      }
+      username: '',
+      password: '',
+      errorMessage: '',
     }),
 
-    mounted () {
-      this.$validator.localize('en', this.dictionary)
-    },
-
     methods: {
-      submit () {
+      submit() {
         this.$validator.validateAll().then(result => {
-            if (result) this.$router.push('/reservations/')
+          let params = new URLSearchParams();
+          params.append('username', this.username);
+          params.append('password', this.password);
+          if (result) {
+            axios.post('http://localhost:8080/api/auth/login', params)
+              .then(
+                response => {
+                  console.log(response.status)
+                  this.$router.push('/reservations/')
+                }
+              )
+              .catch(error => {
+                if (error.response) {
+                  if (error.response.status === 401) {
+                    this.errorMessage = this.$t('error.login_failed')
+                  } else if (error.response.status === 500) {
+                    this.errorMessage = this.$t('error.internal_server_error')
+                  } else {
+                    this.errorMessage = this.$t('error.internal_server_error')
+                  }
+                }
+                console.log(error)
+              })
+          }
         })
       }
     }
